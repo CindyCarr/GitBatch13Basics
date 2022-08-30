7d2c003b5859abc04ad18cbf2052c4d7b71ea969
CindyCarr <cindyparedes001@gmail.com>
2022-08-29 20:12:43 -0400

    temp check basic code2

--- src/test/java/gitBasics.java
+++ src/test/java/gitBasics.java
@@ -2,7 +2,7 @@ public class gitBasics {
     public static void main(String[] args) {
 //        write down a code that checks the temperature from a website and then
 //        prints out that is it a beautiful day or not
-        System.out.println("Today will be a beautiful day");
+        System.out.println("Today will not be a beautiful day");
 
     }
 }
